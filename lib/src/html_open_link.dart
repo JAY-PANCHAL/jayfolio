41ae2713aadcd8b93f28cf1fdc696e38d440ff5c
Jay Panchal <jaypanchal4498@gmail.com>
2025-05-21 18:24:03 +0530

    html open link changed for web

--- lib/src/html_open_link.dart
+++ lib/src/html_open_link.dart
@@ -1,5 +1,8 @@
 import 'dart:html' as html;
 
 void htmlOpenLink(String url) {
-  html.window.open(url, '_blank');
+  final encodedUrl = Uri.encodeFull(url); // Ensures valid format
+  html.window.open(encodedUrl, '_blank');
+
+  // html.window.open(url, '_blank');
 }
